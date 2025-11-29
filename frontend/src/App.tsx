dd1b0fcf79215d248812cdc3210a36ad932a0027
Seba <sebsym96@gmail.com>
2025-11-29 17:36:04 +0100

    initial commit

---
+++ frontend/src/App.tsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <div className="bg-amber-950 w-screen h-screen">piorka</div>
+    </>
+  );
+}
+
+export default App;
